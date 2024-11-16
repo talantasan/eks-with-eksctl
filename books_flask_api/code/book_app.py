8ba5cbd4ae16f6e6f431afcda311d7f301879ab4
talantasan <talant.py@gmail.com>
2024-11-16 03:15:39 +0000

    dfsdf

---
+++ books_flask_api/code/book_app.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+employee_list = [
+    {
+        'id': 1,
+        'name': 'Junai',
+        'occupation': 'IT Engineer'
+    }
+]
+
+@app.route('/')
+def index():
+    return '<p>Hi, welcome to books api.</p>'
+
+@app.route('/<name>')
+def print_name(name):
+    return f"<h3><p>Hello {name}</p><h3>"
+
+@app.route('/employees', methods=['GET', 'POST'])
+def employee():
+    if request.method == 'GET':
+        if len(employee_list) > 0:
+            return jsonify(employee_list)
+        else:
+            return 'Not Found', 404
+        
+    if request.method == 'POST':
+        new_id = employee_list[-1]['id']
+        new_name = request.form['name']
+        new_occupation = request.form['occupation']
+        
+        new_object = {
+            'id': new_id,
+            'name': new_name,
+            'occupation': new_occupation
+        }
+        
+        employee_list.append(new_object)
+        return jsonify(new_object), 201
+        
+        
+
+
+
---
+++ test.py
@@ -0,0 +1,10 @@
+employee_list = [
+    {
+        'id': 1,
+        'name': 'Junai',
+        'occupation': 'IT Engineer'
+    }
+]
+
+
+print(employee_list[-1]['id'])
